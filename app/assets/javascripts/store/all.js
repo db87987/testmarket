be4b4eb17edc5e956debfa38fed7c60fad510fd9
Viacheslav Bobrov <babrovka@gmail.com>
2013-02-18 16:17:18 +0400

    menu slide

--- app/assets/javascripts/store/all.js
+++ app/assets/javascripts/store/all.js
@@ -13,9 +13,18 @@
 //= require store/spree_essential_menus
 //= require_tree .
 //= require store/jquery.carouFredSel-6.1.0-packed
+//= require store/jquery.mousewheel-3.0.6.pack
 
 
 $(document).ready(function(){
+	
+	$("#catalogue-menu > ul > li > a").click(function(){
+		$(this).toggleClass("active").siblings("ul").slideToggle().
+		parent().siblings("li").find("a:first").removeClass("active").siblings("ul").slideUp();
+		;
+		
+		return false;
+	});
 	
 	$("#slider-idx ul").carouFredSel({
 		auto 			: {pauseDuration: 5000}, 
@@ -50,6 +59,38 @@ $(document).ready(function(){
 		$(this).attr('rel', oldImg);
 	});
 	
+	// Минус
+	$(".bb-quantity .minus").click(function(){
+		var thisInput = $(this).siblings("input");
+		var thisInpVal = parseInt($(thisInput).val());
+		if(thisInpVal > 1)
+		{
+			$(thisInput).val(thisInpVal - 1);
+		}
+		return false;
+	});
+	
+	// Плюс
+	$(".bb-quantity .plus").click(function(){
+		var thisInput = $(this).siblings("input");
+		var thisInpVal = parseInt($(thisInput).val());
+		
+		$(thisInput).val(thisInpVal + 1);
+		
+		return false;
+	});
+	
+	// Клик по табам с описаниями в карточке товара
+	$(".ce-tabs ul li a").click(function(){
+		var thisHref = $(this).attr("href");
+		
+		$(this).parent().addClass("active").siblings("li").removeClass("active");
+		$("#" + thisHref).show().siblings("div").hide();
+		
+		return false;
+	});
+	
+
 
 	
  });
