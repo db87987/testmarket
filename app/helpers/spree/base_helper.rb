480c16e381e1d9ca228556cab9f15b9219718635
Viacheslav Bobrov <babrovka@gmail.com>
2013-02-22 20:38:51 +0400

    price format

--- app/helpers/spree/base_helper.rb
+++ app/helpers/spree/base_helper.rb
@@ -111,9 +111,8 @@ module Spree
     def breadcrumbs(taxon, separator="&nbsp;&raquo;&nbsp;")
       return "" if current_page?("/") || taxon.nil?
       separator = raw(separator)
-      crumbs = [content_tag(:li, link_to(t(:home) , root_path) + separator)]
+      crumbs = []
       if taxon
-        crumbs << content_tag(:li, link_to(t(:products) , products_path) + separator)
         crumbs << taxon.ancestors.collect { |ancestor| content_tag(:li, link_to(ancestor.name , seo_url(ancestor)) + separator) } unless taxon.ancestors.empty?
         crumbs << content_tag(:li, content_tag(:span, link_to(taxon.name , seo_url(taxon))))
       else
