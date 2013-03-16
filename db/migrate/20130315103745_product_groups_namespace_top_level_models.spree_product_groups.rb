64852f95c26079ee4015f58b22eb7ff3355c37ca
Viacheslav Bobrov <babrovka@gmail.com>
2013-03-16 12:52:53 +0400

    remove useless migrations

--- db/migrate/20130315103745_product_groups_namespace_top_level_models.spree_product_groups.rb
+++
@@ -1,8 +0,0 @@
-# This migration comes from spree_product_groups (originally 20111007143030)
-class ProductGroupsNamespaceTopLevelModels < ActiveRecord::Migration
-  def change
-    rename_table :product_groups,          :spree_product_groups
-    rename_table :product_groups_products, :spree_product_groups_products
-    rename_table :product_scopes,          :spree_product_scopes
-  end
-end
