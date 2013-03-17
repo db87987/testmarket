ff98e51257f6a18a67f2850b7717f2116179c004
Viacheslav Bobrov <babrovka@gmail.com>
2013-03-17 12:38:27 +0400

    news and articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,11 @@ module ApplicationHelper
     link_to link_text, link_path, :class => class_name
   end
   
+  def span_link(link_text, link_path)
+    if current_page?(link_path)
+      content_tag(:span, link_text)
+    else
+      link_to link_text, link_path
+    end
+  end
 end
