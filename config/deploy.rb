11781df1a3cff4f7d440ffc27fa9b1d8c5feaa02
Viacheslav Bobrov <babrovka@gmail.com>
2013-04-13 13:47:44 +0400

    deploy to new account

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,10 +29,7 @@ require 'bundler/capistrano'
 # сжатых файлов статики при деплое.
 # Если вы не используете assets pipelining в своем проекте,
 # или у вас старая версия rails, закомментируйте эту строку.
-
-
-load File.expand_path('../recipes/assets.rb', __FILE__)
-
+load 'deploy/assets'
 
 # Для удобства работы мы рекомендуем вам настроить авторизацию
 # SSH по ключу. При работе capistrano будет использоваться
@@ -44,7 +41,7 @@ ssh_options[:forward_agent] = true
 
 # Имя вашего проекта в панели управления.
 # Не меняйте это значение без необходимости, оно используется дальше.
-set :application,     "testmarket"
+set :application,     "allmusicmarket"
 
 # Сервер размещения проекта.
 set :deploy_server,   "fluorine.locum.ru"
@@ -52,8 +49,8 @@ set :deploy_server,   "fluorine.locum.ru"
 # Не включать в поставку разработческие инструменты и пакеты тестирования.
 set :bundle_without,  [:development, :test]
 
-set :user,            "hosting_babrovka"
-set :login,           "babrovka"
+set :user,            "hosting_dczvepsrf"
+set :login,           "dczvepsrf"
 set :use_sudo,        false
 set :deploy_to,       "/home/#{user}/projects/#{application}"
 set :unicorn_conf,    "/etc/unicorn/#{application}.#{login}.rb"
@@ -63,12 +60,10 @@ role :web,            deploy_server
 role :app,            deploy_server
 role :db,             deploy_server, :primary => true
 
-
 # Следующие строки необходимы, т.к. ваш проект использует rvm.
 set :rvm_ruby_string, "1.9.3"
 set :rake,            "rvm use #{rvm_ruby_string} do bundle exec rake" 
 set :bundle_cmd,      "rvm use #{rvm_ruby_string} do bundle"
-
 
 # Настройка системы контроля версий и репозитария,
 # по умолчанию - git, если используется иная система версий,
@@ -91,7 +86,7 @@ task :set_current_release, :roles => :app do
     set :current_release, latest_release
 end
 
-set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use #{rvm_ruby_string} do bundle exec unicorn_rails -Dc #{unicorn_conf})"
+  set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use #{rvm_ruby_string} do bundle exec unicorn_rails -Dc #{unicorn_conf})"
 
 
 # - for unicorn - #
