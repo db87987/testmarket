b25efbbc65fb9a87c094b12f80ddbd14c3904146
Viacheslav Bobrov <babrovka@gmail.com>
2013-04-17 13:44:08 +0400

    filter

--- lib/search.rb
+++ lib/search.rb
@@ -5,7 +5,7 @@ class ProductSearch < Spree::Core::Search::Base
             base_scope = base_scope.ascend_by_master_price if @properties[:sort] == 'price'
             base_scope = base_scope.order('name ASC') if @properties[:sort] == 'name'
             base_scope = base_scope.descend_by_popularity if @properties[:sort] == 'popularity'
-            base_scope = base_scope.includes(:taxons).where('"spree_taxons".name = ?', @properties[:brand]) if @properties[:brand]
+            base_scope = base_scope.where('spree_products.name like ?', "#{@properties[:brand]}%") if @properties[:brand]
             base_scope = base_scope.where(count_on_hand: @properties[:status].to_i) if @properties[:status]
             base_scope
           end
