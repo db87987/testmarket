33a944ccf23715600ab7f964591d257af0457c51
Viacheslav Bobrov <babrovka@gmail.com>
2013-04-17 15:14:16 +0400

    filter to partial + profiler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def authorize
+    if current_user.has_spree_role?("admin")
+      Rack::MiniProfiler.authorize_request
+      Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
+    end
+  end
+  
 end
